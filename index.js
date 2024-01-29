c4047260c1d1941041045dd9767f8b45345d022d
Dhruv69408012 <dhruv408012@gmail.com>
2024-01-29 15:58:24 +0530

    Final

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 
-const allowlist = ["https://thyroidlab-do1sm25az-dhruv69408012s-projects.vercel.app/"];
+const allowlist = ["https://thyroidlab-do1sm25az-dhruv69408012s-projects.vercel.app"];
 
 const corsOptions = function (req, callback) {
   if (allowlist.indexOf(req.header("Origin")) !== -1) {
     return callback(null, { origin: true });
   } else {
-    return callback(null, { origin: false });
+    return callback(new Error("Not allowed by CORS"));
   }
 };
 
@@ -25,4 +25,4 @@ app.use("/api/v1/auth", authRouter);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log("server running on port " + PORT));
+app.listen(PORT, () => console.log("Server running on port " + PORT));
