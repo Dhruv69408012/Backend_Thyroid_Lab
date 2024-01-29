428d51b3cb7a442329100bad7c7a73c04c11161b
Dhruv69408012 <dhruv408012@gmail.com>
2024-01-29 15:25:36 +0530

    Final

--- ML-model/predictor.py
+++ ML-model/predictor.py
@@ -3,9 +3,8 @@ import json
 import joblib
 import pandas as pd
 import numpy as np
-from pathlib import Path
+
 input_data = json.loads(sys.argv[1])
-current_working_directory = Path.cwd()
 
 encoding = {
     0: 'PRIMARY HYPORHYROID',
@@ -17,8 +16,6 @@ encoding = {
 }
 
 def cleaning(df):
-    df=df.replace(True,"t")
-    df=df.replace(False,"f")
     df["TSH"] = pd.to_numeric(df["TSH"].replace("?", np.nan))
     df["T3"] = pd.to_numeric(df["T3"].replace("?", np.nan))
     df["TT4"] = pd.to_numeric(df["TT4"].replace("?", np.nan))
@@ -29,21 +26,15 @@ def cleaning(df):
     df = df.replace(["f"], 0)
     return df
 
-try:
-    model = joblib.load("./ML-model/xgmodel.pkl")
-except FileNotFoundError:
-    received = {"condition": "UNKNOWN_ERROR"}
+model = joblib.load("./ML-model/xgmodel.pkl")
 
 df = pd.DataFrame([input_data])
 
 df = cleaning(df)
 
-try:
-    y_pred = model.predict(df)
-    resulting = [encoding[i] for i in y_pred]
-    received = {"condition": resulting[0]}
-except Exception as e:
-    received = {"condition": "UNKNOWN_ERROR"}
+y_pred = model.predict(df)
+resulting = [encoding[i] for i in y_pred]
+received = {"condition": resulting[0]}
 
 
 print(json.dumps(received))
